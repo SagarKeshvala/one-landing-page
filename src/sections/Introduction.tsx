fde29855606dcafc2edc0c6abe9861a4b10606a4
Sagar <sagarkeshwala15@gmail.com>
2025-12-07 19:30:53 +0530

    Completed introduction section animation

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -1,21 +1,56 @@
+"use client";
 import Tag from "@/components/Tag";
+import { useScroll, useTransform } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
-const text = `You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
+const text = `Your creative process deserves better. You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
+const words = text.split(" ");
 
 export default function Introduction() {
+	const scrollTarget = useRef<HTMLDivElement>(null);
+	const { scrollYProgress } = useScroll({
+		target: scrollTarget,
+		offset: ["start end", "end end"],
+	});
+	const [currentWord, setCurrentWord] = useState(0);
+	const wordIndex: any = useTransform(
+		scrollYProgress,
+		[0, 1],
+		[0, words.length]
+	);
+
+	useEffect(() => {
+		wordIndex.on("change", (latest: any) => {
+			setCurrentWord(latest);
+		});
+	}, [wordIndex]);
+
 	return (
 		<section className="py-28 lg:py-40">
 			<div className="container">
-				<div className="flex justify-center">
-					<Tag>Introducing Layers</Tag>
+				<div className="sticky top-20 md:top-28 lg:top-40">
+					<div className="flex justify-center">
+						<Tag>Introducing Layers</Tag>
+					</div>
+					<div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
+						<span className="text-white/15">
+							{words.map((word, index) => (
+								<span
+									key={index}
+									className={twMerge(
+										"transition duration-500",
+										index < currentWord ? "text-white" : "text-white/15"
+									)}
+								>{`${word} `}</span>
+							))}
+						</span>
+						<span className="text-lime-400 block">
+							That's why we built layers
+						</span>
+					</div>
 				</div>
-				<div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
-					<span>Your creative process deserves better.</span>{" "}
-					<span className="text-white/15">{text}</span>
-					<span className="text-lime-400 block">
-						That's why we built layers
-					</span>
-				</div>
+				<div className="h-[150vh]" ref={scrollTarget}></div>
 			</div>
 		</section>
 	);
