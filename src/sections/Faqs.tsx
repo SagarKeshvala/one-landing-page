b2e39ab1574755b888b369a75d7f0e47cbab29ee
Sagar <sagarkeshwala15@gmail.com>
2025-12-07 20:46:28 +0530

    added animations in faqs section

--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Tag from "@/components/Tag";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 const faqs = [
@@ -30,7 +33,7 @@ const faqs = [
 ];
 
 export default function Faqs() {
-	const selectedIndex = 0;
+	const [selectedIndex, setSelectedIndex] = useState(0);
 	return (
 		<section className="py-24">
 			<div className="container">
@@ -44,6 +47,7 @@ export default function Faqs() {
 					{faqs.map((faq, index) => (
 						<div
 							key={faq.question}
+							onClick={() => setSelectedIndex(index)}
 							className="bg-neutral-900 rounded-2xl border border-white/10 p-6"
 						>
 							<div className="flex justify-between items-center">
@@ -60,18 +64,29 @@ export default function Faqs() {
 									strokeLinejoin="round"
 									className={twMerge(
 										"feather feather-plus text-lime-400 flex-shrink-0",
-										selectedIndex === index && "rotate-45"
+										selectedIndex === index &&
+											"rotate-45 transition duration-300"
 									)}
 								>
 									<line x1="12" y1="5" x2="12" y2="19"></line>
 									<line x1="5" y1="12" x2="19" y2="12"></line>
 								</svg>
 							</div>
-							<div
-								className={twMerge("mt-6", selectedIndex !== index && "hidden")}
-							>
-								<p className="text-white/50">{faq.answer}</p>
-							</div>
+							<AnimatePresence>
+								{selectedIndex === index && (
+									<motion.div
+										initial={{ height: 0, marginTop: 0 }}
+										animate={{ height: "auto", marginTop: 24 }}
+										exit={{ height: 0, marginTop: 0 }}
+										className={twMerge(
+											"overflow-hidden",
+											selectedIndex !== index && "hidden"
+										)}
+									>
+										<p className="text-white/50">{faq.answer}</p>
+									</motion.div>
+								)}
+							</AnimatePresence>
 						</div>
 					))}
 				</div>
